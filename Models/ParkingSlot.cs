244aec1610d17af9ab4b308bf5ddc85d37ee3f97
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-11-23 11:29:43 +0100

    Model Classes

---
+++ Models/ParkingSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Defines the info of a parking slot
+/// </summary>
+public class ParkingSlot
+{
+    /// <summary>
+    /// Id of the Parking slot
+    /// </summary>
+    public int ParkingId { get; set; }
+    /// <summary>
+    /// Time when the state of Occupied (boolean) is changed
+    /// </summary>
+    public DateTime SensorDateTime { get; set; }
+    /// <summary>
+    /// States if a parking slot is taken or not
+    /// </summary>
+    public bool Occupied { get; set; }
+}
---
+++ Models/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApi_REST.Models
+{
+    /// <summary>
+    /// Assigning a sensor to a parking slot
+    /// </summary>
+    public class Sensor
+    {
+        /// <summary>
+        /// Id of the sensor
+        /// </summary>
+        public int SensorId { get; set; }
+        /// <summary>
+        /// Id of the parking slot
+        /// </summary>
+        public int ParkingId { get; set; }
+    }
+}
