25b27489215e99aa0109d704602becb305bb6b08
Nohely <ngedeon14@yahoo.com>
2021-11-29 14:13:05 +0100

    secrets

---
+++ ParkingWebApi_REST/Secrets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_REST
+{
+    public class Secrets
+    {
+        public static readonly string ConnectionString =
+            "Server=tcp:sensor-sql-server.database.windows.net,1433;Initial Catalog=Sensor_DataBase;Persist Security Info=False;User ID=SensorAdmin;Password=Parking21;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+       
+
+    }
+}
