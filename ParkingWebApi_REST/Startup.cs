4ce5d1a89c29169c1ba0323c402258fe7a934cc3
Niko D <nikodojan@live.de>
2021-12-07 11:20:16 +0100

    xml comments for swagger registered in startup.cs

--- ParkingWebApi_REST/Startup.cs
+++ ParkingWebApi_REST/Startup.cs
@@ -9,7 +9,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using WebApi_REST.Managers;
 using WebApi_REST.Models;
@@ -43,9 +45,17 @@ namespace WebApi_REST
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi_REST", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                // Requires XML documentation and XML file.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
             //Registering a service and telling the Data access layer what database to access.
             services.AddDbContext<ParkingDbContext>(options => options.UseSqlServer(Secrets.ConnectionString));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,9 +64,10 @@ namespace WebApi_REST
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi_REST v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi_REST v1"));
 
             app.UseRouting();
             app.UseCors("AllOrigins");
